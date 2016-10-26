e17a6d1f54a312ab3b2d9f0bb8dea13976c3d70d
Dima <d.gachok@gmail.com>
2016-10-27 00:27:23 +0300

    footer url

--- wp-content/themes/mikhrin-site/footer.php
+++ wp-content/themes/mikhrin-site/footer.php
@@ -17,12 +17,12 @@
 		<div class="container">
 			<ul class="footer__menu">
 				<li class="active"><a href="">Про MD Event</a></li>
-				<li><a href="">Новини</a></li>
-				<li><a href="">Послуги</a></li>
-				<li><a href="">Фото</a></li>
-				<li><a href="">Клієнти</a></li>
-				<li><a href="">Партнери</a></li>
-				<li><a href="">Контакти</a></li>
+				<li><a href="<?php echo get_home_url().'/news/'?>">Новини</a></li>
+				<li><a href="<?php echo get_home_url().'/services-horeca/'?>">Послуги</a></li>
+				<li><a href="<?php echo get_home_url().'/photos/'?>">Фото</a></li>
+				<li><a href="<?php echo get_home_url().'/customers/'?>">Клієнти</a></li>
+				<li><a href="<?php echo get_home_url().'/partners/'?>">Партнери</a></li>
+				<li><a href="<?php echo get_home_url().'/contacts/'?>">Контакти</a></li>
 			</ul>
 			<div class="brand_block">
 				<div class="brand_block__brand_item"><img src="/wp-content/themes/mikhrin-site/images/footer/footer_group.png"></div>
